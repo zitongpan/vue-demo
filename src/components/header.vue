d36b2370005cba0bf6f77c019b047c8a4cb76aeb
panzitong <453587561@qq.com>
2019-04-18 14:47:41 +0800

    添加nav

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,13 +2,22 @@
   <el-container class="header">
     <el-header>
       <el-row>
-        <el-col :sm="6" :xs="24" class="logo">鲸鱼智能
-          <span class="iconfont fr f_6 cp" :class="{'hide' : showMenu}" @click="showMenu = !showMenu">&#xe602;</span>
-          <span class="iconfont fr f_6 cp" :class="{'hide' : !showMenu}" @click="showMenu = !showMenu">&#xe631;</span>
+        <el-col :sm="6" :xs="24" class="logo">
+          鲸鱼智能
+          <span
+            class="iconfont fr f_6 cp"
+            :class="{'hide' : showMenu}"
+            @click="showMenu = !showMenu"
+          >&#xe602;</span>
+          <span
+            class="iconfont fr f_6 cp"
+            :class="{'hide' : !showMenu}"
+            @click="showMenu = !showMenu"
+          >&#xe631;</span>
         </el-col>
         <el-col tag="ul" :sm="18" :xs="24" :class="{'hide' : showMenu}">
-          <li>首页</li>
-          <li>策略集市</li>
+          <router-link tag="li" :to="{name:'home'}" class="cp">首页</router-link>
+          <router-link tag="li" :to="{name:'about'}" class="cp">策略</router-link>
         </el-col>
       </el-row>
     </el-header>
@@ -16,13 +25,13 @@
 </template>
 <script>
 export default {
-  name:'myHeader',
-  data(){
-    return{
-      showMenu:false,
-    }
+  name: 'myHeader',
+  data() {
+    return {
+      showMenu: false,
+    };
   },
-}
+};
 </script>
 
 <style lang="stylus" scoped>
@@ -32,32 +41,40 @@ export default {
     line-height 60px
     background #91ddcc
     font-size 16px
+
     .logo
       span
         display none
+
     .el-row
       width 100%
+
       ul.el-col
         display flex
         justify-content flex-end
+
         li
           padding 0 20px
+
 @media screen and (max-width: 768px)
   .header /deep/
     .el-header
       height auto !important
+
       .logo
         text-align left
+
         span
           display block
+
           &.hide
             display none
+
       .el-row
         ul.el-col
           line-height 50px
           display none
+
           &.hide
             display unset
-
 </style>
-
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div>??</div>
+    welcome to my home!
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
